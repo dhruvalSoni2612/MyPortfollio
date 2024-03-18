7003f6b7fcfe0d674ccd4d48091901eaf03552db
dhruval soni <dhruvalsoni717@gmail.com>
2024-03-18 10:24:48 +0530

    change

--- index.js
+++ index.js
@@ -2,4 +2,7 @@
 document.getElementById("menu-toggle").addEventListener("change", function() {
   var navList = document.querySelector('.nav_list');
   navList.style.display = this.checked ? 'flex' : 'none';
+  document.body.addEventListener("click", function(){
+    navList.style.display =  "none";
+  })
 });

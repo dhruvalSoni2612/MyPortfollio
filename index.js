77d4254f307c2ecfee84f1c526327e8437e8dce0
dhruval soni <dhruvalsoni717@gmail.com>
2024-04-03 13:35:59 +0530

    add

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// Toggle navigation menu on hamburger icon click
-document.getElementById("menu-toggle").addEventListener("change", function() {
-  var navList = document.querySelector('.nav_list');
-  navList.style.display = this.checked ? 'flex' : 'none';
-  document.body.addEventListener("click", function(){
-    navList.style.display =  "none";
-  })
+document.getElementById("menu-toggle").addEventListener("change", function () {
+  var navList = document.querySelector(".nav_list");
+  navList.style.display = this.checked ? "flex" : "none";
+  document.body.addEventListener("click", function () {
+    navList.style.display = "none";
+  });
 });

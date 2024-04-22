84309c92bb05b04ccf9c1b4a099f40176092264c
dhruval soni <dhruvalsoni717@gmail.com>
2024-04-22 17:11:43 +0530

    add

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ document.getElementById("menu-toggle").addEventListener("change", function () {
     navList.style.display = "none";
   });
 });
+
+const titles = [
+  { title: "Web Developer", role: "Web Developer" },
+  { title: "Frontend Developer", role: "Frontend Developer" },
+];
+
+let currentIndex = 0;
+
+function updateTitle() {
+  const titleSpan = document.getElementById("titleText");
+  const roleSpan = document.getElementById("roleText");
+  titleSpan.innerText = `I'm `;
+  roleSpan.innerText = titles[currentIndex].title;
+  currentIndex = (currentIndex + 1) % titles.length;
+  setTimeout(updateTitle, 2000);
+}
+
+updateTitle();
